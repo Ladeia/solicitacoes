5a151e7325857e08364936eb27933bc9a5da2f6d
Antonio Fernando Santos Ladeia <contato@antonioladeia.com>
2017-06-01 08:29:33 -0300

    remove all classes from html and personal data from backend

---
+++ php/contactForm.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST['name']) && isset($_POST['contactEmail']) && isset($_POST['message']) && isset($_POST['title'])){
+	
+	$email_remetente = $_POST['contactEmail'];
+
+	$headers  = "MIME-Version: 1.1\n";
+	$headers .= "Content-type: text/plain; charset=UTF-8\n";
+	$headers .= "From: $email_remetente\r\n"; 
+	$headers .= "Return-To: noreply.webform@reconcavo.org.br\r\n"; 
+	$headers .= "Return-Path: noreply.webform@reconcavo.org.br\r\n";
+
+	$to       = "";
+	$subject  = $_POST['title'];
+	$message  = $_POST['message'];
+
+	if(mail($to, $subject, $message, $headers)){ echo "Enviado com sucesso!"; }else{ echo "# Falha no envio."; };
+}
+?>
